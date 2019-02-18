2a3910bd85a8a8dfa9a3e1e6fcfec788b64240dd
zj-zhang <zzj.zju@gmail.com>
2019-02-17 17:27:12 -0800

    update get_data with sftp url

--- Darts_DNN/Darts_DNN/Darts_get_data.py
+++ Darts_DNN/Darts_DNN/Darts_get_data.py
@@ -25,17 +25,28 @@ def parser(args):
 	data = list(set(args.data))
 	event_type = args.event_type
 	outdir = args.out_dir
+	data_config = config.DOWNLOAD_CONFIG[args.sp]
 
 	# perform downloading
 	for d in data:
-		if not event_type in config.CURRENT_AVAILABLE_DATA[d]:
+		if not event_type in data_config[d]:
 			logger.info("data '%s' is not available in event type '%s' for version '%s'"%(d, event_type, config.CURRENT_VERSION) )
 			continue
-		url = config.CURRENT_AVAILABLE_DATA[d][event_type]['url']
-		md5 = config.CURRENT_AVAILABLE_DATA[d][event_type]['md5sum']
-		md5 = None if md5=='None' else md5
-		basename = os.path.basename(url)
-		outfn = os.path.join(outdir, basename)
-		logger.info("dowloading '%s' to '%s'" % (url, outfn) )
-		download.download_with_resume(url, outfn, md5)
+		url_list = data_config[d][event_type]['url']
+		md5_list = data_config[d][event_type]['md5sum']
+		output_list = data_config[d][event_type]['output']
+		url_list = url_list if isinstance(url_list, list) else [url_list]
+		md5_list = md5_list if isinstance(md5_list, list) else [md5_list]
+		output_list = output_list if isinstance(output_list, list) else [output_list]
+		for url, md5, outfn in zip(url_list, md5_list, output_list):
+			md5 = None if md5=='None' else md5
+			if outdir:
+				basename = os.path.basename(url)
+				outfn = os.path.join(outdir, basename)
+			if outfn.startswith('~/'):
+				outfn = os.path.join(os.path.expanduser('~'), outfn.lstrip('~/'))
+			if not os.path.isdir(os.path.dirname(outfn)):
+				os.makedirs(os.path.dirname(outfn))
+			logger.info("dowloading '%s' to '%s'" % (url, outfn) )
+			download.download_with_resume(url, outfn, md5)
 	return
--- Darts_DNN/Darts_DNN/download.py
+++ Darts_DNN/Darts_DNN/download.py
@@ -6,11 +6,15 @@ from https://gist.github.com/mjohnsullivan/9322154
 """
 
 import os.path
-import urllib2
+try:
+    import urllib2
+except:
+    import urllib.request as urllib2
 import shutil
 import hashlib
 import logging
 import ssl
+from tqdm import tqdm
 
 logger = logging.getLogger('Darts_DNN.get_data')
 
@@ -51,8 +55,11 @@ def download_with_resume(url, file_path, hash=None, timeout=10):
     file_size = -1
     gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)  
     try:
+        file_size = int(urllib2.urlopen(url).info().get('Content-Length', -1))        
+    except:
         file_size = int(urllib2.urlopen(url, context=gcontext).info().get('Content-Length', -1))
-        logger.debug('File size is %s' % file_size)
+    logger.debug('File size is %s' % file_size)
+    with tqdm(total=file_size, unit='B', unit_scale=True, unit_divisor=1024) as pbar:
         while first_byte < file_size:
             last_byte = first_byte + block_size \
                 if first_byte + block_size < file_size \
@@ -61,19 +68,22 @@ def download_with_resume(url, file_path, hash=None, timeout=10):
             # create the request and set the byte range in the header
             req = urllib2.Request(url)
             req.headers['Range'] = 'bytes=%s-%s' % (first_byte, last_byte)
-            data_chunk = urllib2.urlopen(req, timeout=timeout, context=gcontext).read()
+            try:
+                data_chunk = urllib2.urlopen(req, timeout=timeout).read()
+            except:
+                data_chunk = urllib2.urlopen(req, timeout=timeout, context=gcontext).read()
             # Read the data from the URL and write it to the file
             with open(tmp_file_path, 'ab') as f:
                 f.write(data_chunk)
             first_byte = last_byte + 1
-    except IOError as e:
-        logger.debug('IO Error - %s' % e)
-    finally:
-        # rename the temp download file to the correct name if fully downloaded
-        if file_size == os.path.getsize(tmp_file_path):
-            # if there's a hash value, validate the file
-            if hash and not validate_file(tmp_file_path, hash):
-                raise Exception('Error validating the file against its MD5 hash')
-            shutil.move(tmp_file_path, file_path)
-        elif file_size == -1:
-            raise Exception('Error getting Content-Length from server: %s' % url)
+            # update pbar
+            pbar.update( block_size )
+
+    # rename the temp download file to the correct name if fully downloaded
+    if file_size == os.path.getsize(tmp_file_path):
+        # if there's a hash value, validate the file
+        if hash and not validate_file(tmp_file_path, hash):
+            raise Exception('Error validating the file against its MD5 hash')
+        shutil.move(tmp_file_path, file_path)
+    elif file_size == -1:
+        raise Exception('Error getting Content-Length from server: %s' % url)
